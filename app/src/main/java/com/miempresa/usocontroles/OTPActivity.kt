e62eb07a847f40378a5fc4ce76a23920533a1793
carlos quiroz <carlos.quiroz@tecsup.edu.pe>
2023-06-03 17:55:51 -0500

    cambios de extensions por binding

--- app/src/main/java/com/miempresa/usocontroles/OTPActivity.kt
+++ app/src/main/java/com/miempresa/usocontroles/OTPActivity.kt
@@ -39,20 +39,3 @@ class OTPActivity : AppCompatActivity() {
 
     }
 }
-/* continueBtnOtp.setOnClickListener(){
-              var punto1 = otp_view1.text.toString()
-              var punto2 = otp_view2.text.toString()
-              var punto3 = otp_view3.text.toString()
-              var punto4 = otp_view4.text.toString()
-              var punto5 = otp_view5.text.toString()
-              var punto6 = otp_view6.text.toString()
-
-
-            if (punto1 == "6" && punto2 == "2" && punto3 == "8" && punto4 == "6" && punto5 == "8" && punto6 == "9") {
-                val intent = Intent(this, LogrosActivity::class.java)
-                startActivity(intent)
-            } else {
-                Toast.makeText(this, "Su codigo de verifacion es incorrecto", Toast.LENGTH_SHORT).show()
-                       }
-
-                 }*/
